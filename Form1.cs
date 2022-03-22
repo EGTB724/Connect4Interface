f90b442bc692a63aa40ff2e16006b56cfee65a17
Nathan Cauley <nacaule@siue.edu>
2022-03-21 20:15:28 -0500

    I made a change

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace Connect4Interface
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender,EventArgs e) {
+
+        }
+
+        //This is Nathan
     }
 }
